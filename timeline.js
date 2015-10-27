3d30da8954844b84b9701b151f5892160cdd1bbc
Paal Joergensen <pal.jorgensen@gmail.com>
2015-10-27 13:57:13 +0100

    Improved the validation of input data [HFJ-1222]

--- timeline.js
+++ timeline.js
@@ -41,6 +41,26 @@
   };
 
   /**
+   * Check if data provided is valid.
+   * @method validate
+   * @return {boolean} Valid or not
+   */
+  C.prototype.validate = function () {
+    if (this.options.timeline.date === undefined || this.options.timeline.date.length === 0) {
+      return false;
+    }
+
+    for (var i = 0; i < this.options.timeline.date.length; i++) {
+      var element = this.options.timeline.date[i];
+      if (element.startDate === undefined || element.headline === undefined) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  /**
    * Attatch the Timeline HTML to a given target.
    **/
   C.prototype.attach = function ($container) {
@@ -49,17 +69,25 @@
     // Need to set this to make timeline behave correctly:
     window.jQuery = $;
 
-    createStoryJS({
-      type: 'timeline',
-      width: '100%',
-      height: '600',
-      source: this.options,
-      lang: this.options.timeline.language,
-      start_zoom_adjust: this.options.timeline.defaultZoomLevel,
-      embed_id: 'h5p-timeline',
-      css: H5P.getLibraryPath('TimelineJS-1.0') + '/css/timeline.css',
-      js: H5P.getLibraryPath('TimelineJS-1.0') + '/js/timeline-min.js'
-    });
+    if (this.validate()) {
+      createStoryJS({
+        type: 'timeline',
+        width: '100%',
+        height: '600',
+        source: this.options,
+        lang: this.options.timeline.language,
+        start_zoom_adjust: this.options.timeline.defaultZoomLevel,
+        embed_id: 'h5p-timeline',
+        css: H5P.getLibraryPath('TimelineJS-1.0') + '/css/timeline.css',
+        js: H5P.getLibraryPath('TimelineJS-1.0') + '/js/timeline-min.js'
+      });
+    }
+    else {
+      $container.append($('<div>', {
+        html: 'Missing mandatory data - not able to draw the timeline. Please open this H5P in the editor, and make the necessary changes.',
+        'class': 'h5p-timeline-data-not-valid'
+      }));
+    }
   };
 
   return C;
