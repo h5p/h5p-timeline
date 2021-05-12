6f2885f72cb67b020ab598ea47aaf76cd744e72f
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-05-12 19:21:51 +0200

    Update to TimelineJS3

--- timeline.js
+++ timeline.js
@@ -108,15 +108,109 @@
     if (self.validate()) {
       // Load library.json - need to inform TimelineJS which version it is
       $.getJSON(self.getLibraryFilePath('library.json'), function (data) {
-        new TimelineJS({
-          type: 'timeline',
+
+        var datas = {
+          "title": {
+            "text": {
+              "headline": self.options.timeline.headline,
+              "text": self.options.timeline.text
+            }
+          },
+          "events": [],
+          "eras": []
+        };
+
+        if (self.options.timeline.asset !== undefined && self.options.timeline.asset.media) {
+          datas.title.media = {
+            "url": self.options.timeline.asset.media,
+            "caption": self.options.timeline.asset.caption,
+            "credit": self.options.timeline.asset.credit,
+            "thumbnail": self.options.timeline.asset.thumbnail
+          };
+        }
+
+        // convert date to TimelineJS3
+        self.options.timeline.date && self.options.timeline.date.forEach(function (event2) {
+          var event3 = {
+            "text": {
+              "headline": event2.headline,
+              "text": event2.text
+            }
+          };
+          if (event2.asset !== undefined && event2.asset.media) {
+            event3.media = {
+              "url": event2.asset.media,
+              "caption": event2.asset.caption,
+              "credit": event2.asset.credit
+            };
+          }
+          var startDateRegexResult = event2.startDate.match(/^(?<year>-?\d{1,})(,(?<month>1[012]|[0]?[1-9])(,(?<day>[12][0-9]|3[01]|[0]?[1-9]))?)?$/);
+          if (startDateRegexResult) {
+            event3.start_date = {
+              "month": startDateRegexResult.groups.month,
+              "day": startDateRegexResult.groups.day,
+              "year": startDateRegexResult.groups.year
+            };
+
+            var endDateRegexResult = event2.endDate.match(/^(?<year>-?\d{1,})(,(?<month>1[012]|[0]?[1-9])(,(?<day>[12][0-9]|3[01]|[0]?[1-9]))?)?$/);
+            if (endDateRegexResult) {
+              event3.end_date = {
+                "month": endDateRegexResult.groups.month,
+                "day": endDateRegexResult.groups.day,
+                "year": endDateRegexResult.groups.year
+              };
+            }
+
+            event3.group = event2.tag;
+
+            datas.events.push(event3);
+          }
+        });
+
+        // convert eras to TimelineJS3
+        self.options.timeline.era && self.options.timeline.era.forEach(function (era2) {
+          var era3 = {
+            "text": {
+              "headline": era2.headline,
+              "text": era2.text
+            }
+          };
+        
+          var startDateRegexResult = era2.startDate.match(/^(?<year>-?\d{1,})(,(?<month>1[012]|[0]?[1-9])(,(?<day>[12][0-9]|3[01]|[0]?[1-9]))?)?$/);
+          if (startDateRegexResult) {
+            era3.start_date = {
+              "month": startDateRegexResult.groups.month,
+              "day": startDateRegexResult.groups.day,
+              "year": startDateRegexResult.groups.year
+            };
+
+            var endDateRegexResult = era2.endDate.match(/^(?<year>-?\d{1,})(,(?<month>1[012]|[0]?[1-9])(,(?<day>[12][0-9]|3[01]|[0]?[1-9]))?)?$/);
+            if (endDateRegexResult) {
+              era3.end_date = {
+                "month": endDateRegexResult.groups.month,
+                "day": endDateRegexResult.groups.day,
+                "year": endDateRegexResult.groups.year
+              };
+            }
+
+            datas.eras.push(era3);
+          }
+        });
+
+        var config = {
           width: '100%',
           height: '100%',
-          source: self.options,
-          lang: self.options.timeline.language,
-          start_zoom_adjust: self.options.timeline.defaultZoomLevel,
-          embed_id: 'h5p-timeline'
-        }, data.preloadedDependencies[0].majorVersion, data.preloadedDependencies[0].minorVersion);
+          language: self.options.timeline.language,
+          scale_factor: 1 / 3 * Math.pow(2, self.options.timeline.defaultZoomLevel)
+        };
+
+        // clean undefined
+
+        datas = $.extend(true, {}, datas);
+
+        delete datas.title.media;
+
+        new TimelineJS('h5p-timeline', datas, config, data.preloadedDependencies[0].majorVersion, data.preloadedDependencies[0].minorVersion);
 
         // Add background image if any:
         if (self.options.timeline.backgroundImage !== undefined) {
